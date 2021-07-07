876ea5a8665e77f871952cfde65d644b6dffe195
Beau Barrier <beaubarrier@gmail.com>
2021-07-06 19:30:24 -0700

    Added ui interface for main homepage.

--- public/js/index.js
+++ public/js/index.js
@@ -54,3 +54,8 @@ backBtn.on("click", function back(e) {
     backBtn.css("display", "none");
 
 })
+
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)
+})
