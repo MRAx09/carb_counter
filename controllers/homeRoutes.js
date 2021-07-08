84b99b1b0c1aa235a4ae1a254e9dfa1fdcd033ff
Melina Boedecker <melinamboedecker@gmail.com>
2021-07-07 19:19:11 -0700

    add get route in homeroutes for getting all favorite foods

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,23 @@ const { Food, Meal, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
-    //what should we have for home page? different view for logged in vs not logged in
+    //Home page shoud show list of favorite foods, might also need a route for current meal.
+    //do we need separate table for current meal or should we use local storage for that?
+    const favData = await Food.findAll({
+      where: {
+        favorite: true
+      }
+    });
+
+    const favorites = favData.map((food) => food.get({ plain: true }));
+    console.log(favorites);
+
+    // res.render('homepage', {
+    //   ...favorites,
+    //   logged_in: req.session.logged_in
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
