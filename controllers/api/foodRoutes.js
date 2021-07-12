3d5196872855cc2ab9acf309e2c20129af6b3242
Matt Linder <mjlinder218@gmail.com>
2021-07-11 21:22:46 -0700

    Fixed an issue with an ID in the delete /:id route

--- controllers/api/foodRoutes.js
+++ controllers/api/foodRoutes.js
@@ -16,11 +16,12 @@ router.post('/', withAuth, async (req, res) => {
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
+  // console.log('In delete router');
   try {
     const foodData = await Food.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
+        // user_id: req.session.user_id,
       },
     });
 
